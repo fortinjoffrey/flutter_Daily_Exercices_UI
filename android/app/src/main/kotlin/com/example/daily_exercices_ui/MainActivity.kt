d23d28e1e8e24edae9856b8d9a382f89d972b21a
Joffrey Fortin <fortinjoffrey@gmail.com>
2020-07-13 16:10:30 +0200

    Upload flutter new project files

---
+++ android/app/src/main/kotlin/com/example/daily_exercices_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.daily_exercices_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: Text('Hello wolrd'),
+    ));
+  }
+}
