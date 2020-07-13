2b1583520a1a2df0344d6dd53607b1a19e517e66
Joffrey Fortin <fortinjoffrey@gmail.com>
2020-07-13 17:39:18 +0200

    Implement home UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:daily_exercices_ui/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'widgets/bottom_nav_bar.dart';
+import 'widgets/category_card.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +14,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: 'Cairo',
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
+      ),
       home: Home(),
     );
   }
@@ -20,9 +31,99 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-      child: Text('Hello wolrd'),
-    ));
+    var size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      bottomNavigationBar: BottomNavBar(),
+      body: Stack(
+        children: [
+          Container(
+            height: size.height * .45,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5CEB8),
+              image: DecorationImage(
+                image: AssetImage('assets/images/undraw_pilates_gpdb.png'),
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF2BEA1),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset('assets/icons/menu.svg'),
+                    ),
+                  ),
+                  Text(
+                    'Good Morning\nJohn',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4
+                        .copyWith(fontWeight: FontWeight.w900),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 30.0),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30.0, vertical: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(29.5),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                        icon: SvgPicture.asset('assets/icons/search.svg'),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: .85,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: [
+                        CategoryCard(
+                          title: 'Diet Recommandation',
+                          svgPath: 'assets/icons/Hamburger.svg',
+                          onPressed: () {},
+                        ),
+                        CategoryCard(
+                          title: 'Kegel Exercises',
+                          svgPath: 'assets/icons/Exercises.svg',
+                          onPressed: () {},
+                        ),
+                        CategoryCard(
+                          title: 'Meditation',
+                          svgPath: 'assets/icons/Meditation.svg',
+                          onPressed: () {},
+                        ),
+                        CategoryCard(
+                          title: 'Yoga',
+                          svgPath: 'assets/icons/yoga.svg',
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../constants.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          BottomNavItem(
+            title: "Today",
+            svgPath: 'assets/icons/calendar.svg',
+          ),
+          BottomNavItem(
+            title: "All Exercises",
+            svgPath: 'assets/icons/gym.svg',
+            isActive: true,
+          ),
+          BottomNavItem(
+            title: "Settings",
+            svgPath: 'assets/icons/settings.svg',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavItem extends StatelessWidget {
+  final String svgPath;
+  final String title;
+  final Function onPressed;
+  final bool isActive;
+
+  const BottomNavItem({
+    Key key,
+    this.svgPath,
+    this.title,
+    this.onPressed,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          SvgPicture.asset(
+            svgPath,
+            color: isActive ? kActiveIconColor : kTextColor,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              color: isActive ? kActiveIconColor : kTextColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
